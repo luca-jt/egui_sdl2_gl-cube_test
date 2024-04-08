0b9356ea46340ae6f48dfb86fdacbc9389372b9d
luca-jt <random.lulu@die-genossen.de>
2024-04-08 22:32:44 +0200

    prettier ui

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern "C"
 }
 
 use eframe::*;
-use egui::{CentralPanel, Response, Ui};
+use egui::{vec2, CentralPanel, Color32, FontId, Response, RichText, Rounding, Ui, FontFamily};
 
 
 struct TPGApp {}
@@ -16,17 +16,37 @@ impl eframe::App for TPGApp
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame)
     {
         CentralPanel::default().show(ctx, |ui: &mut Ui| {
-            ui.label("text placeholder");
 
-            let button_resp: Response = ui.button("test button");
+            ui.vertical_centered(|ui: &mut Ui| {
 
-            if button_resp.clicked()
-            {
-                let test_int;
-                unsafe { test_int = test_func(); }
+                ui.add_space(80.0);
 
-                println!("{}", test_int.to_string());
-            }
+                ui.label(RichText::new("Test CMake Link")
+                    .font(FontId::new(20.0, FontFamily::Monospace))
+                    .underline()
+                );
+
+                ui.add_space(80.0);
+
+                let button = egui::Button::new(RichText::new("call C++ function")
+                                                .strong()
+                                                .font(FontId::new(16.0, FontFamily::Monospace)))
+                                    .fill(Color32::from_rgb(0, 255, 255))
+                                    .rounding(Rounding::same(100.0))
+                                    .min_size(vec2(200.0, 200.0));
+                                
+                let button_resp: Response = ui.add(button);
+
+                if button_resp.clicked()
+                {
+                    let test_int;
+                    unsafe { test_int = test_func(); }
+
+                    println!("{}", test_int.to_string());
+                }
+
+            });
+
         });
     }
 }
